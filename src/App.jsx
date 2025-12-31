8d88379e8edeea6bc65f5660f191e68d3fdc31e3
Amal Kishor <amalkishor.social@gmail.com>
2025-12-31 17:36:28 +0530

    scroll up smootly

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/Footer";
+import useScrollToTop from "./hooks/useScrollToTop";
 
 const App = () => {
+  useScrollToTop();
   return (
     <>
       <Navbar />
